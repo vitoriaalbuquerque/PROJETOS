62042a0d0424c5c6b1fc62fce2438ccea9ea10f1
vitoriaalbuquerque <vitoriabarbosa1998@hotmail.com>
2023-03-14 15:08:18 -0300

    exercicio js

---
+++ js/exercicio9.js
@@ -0,0 +1,13 @@
+const numberOne = 11
+const numberTwo = 5
+const numberThree = 25
+const numberFour = 7
+
+if(numberOne % 2 === 0 && numberTwo % 2 === 0 && numberThree % 2 === 0 && numberFour % 2 === 0 ){
+    console.log("Todos são pares")
+} else if(numberOne % 2 !== 0 && numberTwo % 2 !== 0 && numberThree % 2 !== 0 && numberFour % 2 !== 0 ){
+    console.log("Todos são ímpares")
+} else{
+console.log("Tudo misturado, ímpares e pares")
+}
+
